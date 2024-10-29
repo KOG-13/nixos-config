902a41df689e49a41080041c98f2e70c9ef1afe5
Kogara13 <kieranogara@gmail.com>
2024-10-29 14:05:57 -0400

    fixing issues with updating

--- homes/desktop/zsh.nix
+++ homes/desktop/zsh.nix
@@ -6,7 +6,7 @@
 			in {
 
 			rebuild = "sudo nixos-rebuild switch --flake ${flakeDir}#desktop";
-			update = "nix flake update ${flakeDir}";
+			update = "sudo nix flake update /home/kieran/nix/";
 			upgrade = "sudo nixos-rebuild switch --upgrade --flake ${flakeDir}#desktop";
 			#hms = "home-manager switch --flake ${flakeDir}#desktop";
 
