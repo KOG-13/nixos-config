e1d889f6f3c9b2a17c4c205b0c7f890e5197224c
Kogara13 <kieranogara@gmail.com>
2024-12-20 21:19:31 -0500

    xserver changes

--- common/systems/modules/xserver.nix
+++ common/systems/modules/xserver.nix
@@ -30,3 +30,11 @@ services = {
     desktopManager.plasma6.enable = true;
 };
 }
+
+
+services = {
+    xserver.enable = true;
+    displayManager.sddm.enable = true;
+    displayManager.sddm.wayland.enable = true;
+    desktopManager.plasma6.enable = true;
+};
