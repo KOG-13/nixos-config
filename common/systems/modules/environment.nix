95332aab9cdad3db97b7d9e4f2777a2344796785
Kogara13 <kieranogara@gmail.com>
2025-05-06 21:54:59 -0400

    additions to environment variables and starship

--- common/systems/modules/environment.nix
+++ common/systems/modules/environment.nix
@@ -4,6 +4,17 @@
         WLR_NO_HARDWARE_CURSORS = "1";
         # Hint electron apps to use wayland
         NIXOS_OZONE_WL = "1";
+        # XDG_BASE_DIR variables
+        XDG_CACHE_HOME = "$HOME/.cache";
+        XDG_CONFIG_HOME = "$HOME/.config";
+        XDG_DATA_HOME = "$HOME/.local/share";
+        XDG_STATE_HOME = "$HOME/.local/state";
+
+        XDG_BIN_HOME = "$HOME/.loca/bin";
+
+        # GO config
+        GOPATH = "$XDG_DATA_HOME/go";
+        GOMODCACHE = "$XDG_CACHE_HOME/go/mod";
     
         /*
         Todo list
@@ -16,7 +27,7 @@
 
     };
 
-    environment.variables = {
-		    EDITOR = "nvim";
+    environment.variables = { 
+		    EDITOR = "nvim"; 
 	  };
 }
