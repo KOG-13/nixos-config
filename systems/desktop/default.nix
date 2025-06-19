76a8a14f1112920547cb47d6a323a6c2476bf896
Kogara13 <kieranogara@gmail.com>
2025-06-19 19:50:32 -0400

    reverted to dynamic ip address on system76. Included multiple Wifi profiles.

--- systems/desktop/default.nix
+++ systems/desktop/default.nix
@@ -9,14 +9,6 @@
     # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
     networking = {
         hostName = "desktop";
-        /*
-        interfaces.eth0.ipv4.addresses = [{ 
-            address = "192.168.1.75";
-            prefixLength =24;
-        }];
-        defaultGateway = "192.168.1.1";
-        nameservers = [ "8.8.8.8" ];
-        */
     };
     # Configure network proxy if necessary
     # networking.proxy.default = "http://user:password@proxy:port/";
