e3a8dbd00881a1507b910550896e412bb73fdbea
Kogara13 <kieranogara@gmail.com>
2025-06-20 21:12:53 -0400

    disabled kwallet on git push

--- common/homes/modules/git.nix
+++ common/homes/modules/git.nix
@@ -3,6 +3,11 @@
 		    enable = true;
 		    userName = "Kogara13";
 		    userEmail = "kieranogara@gmail.com";
-		    extraConfig.init.defaultBranch = "main";
+		    #extraConfig.init.defaultBranch = "main";
+        # Use `cache` or `store`, or none
+        extraConfig = {
+          init.defaultBranch = "main";
+          credential.helper = "cache"; # Or "store", or "" (empty string to disable entirely)
+        };
 	  };
 }
